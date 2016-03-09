9df985c9e1431675cea78b54e952d079a1e4dc82
WillMon <w.montero34@gmail.com>
2016-03-08 18:02:16 -0600

    Gat a better understaning of what C# is

--- Assets/Scripts/Interface.cs
+++ Assets/Scripts/Interface.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 
 // Vector 3 
-
-
 // interface that holds my "Inhearted Functions"
 public interface IStats
 {
@@ -17,7 +16,7 @@ public interface IStats
 public interface IAbilities
 {
     void Stealth();
-    void Health(int m_healthbar);
+    int Health(int m_healthbar);
     void Movement(Vector3 s_move);
 
 }
@@ -28,16 +27,27 @@ public class Ninja : MonoBehaviour, IAbilities, IStats
     public void Stealth()
     {
         Console.WriteLine(".... Shhhhhhh");
+        
         return;
     }
-    public void Health(int m_healthbar)
+    public int Health(int m_healthbar)
     {
-        m_health = m_healthbar;
+        health = m_healthbar;
+        return health;
     }
 
-    public void Movement(Vector3 s_move)
+    public void  Movement(Vector3 s_move)
     {
-        
+        Vector3 MovementTracker = new Vector3();
+        Vector3 Collision = new Vector3();
+
+        MovementTracker.x = s_move.x - Time.deltaTime;
+        MovementTracker.y = s_move.y - Time.deltaTime;
+        MovementTracker.z = s_move.z - Time.deltaTime;
+        if (MovementTracker == Collision)
+        {
+            Console.WriteLine("They Have Collided");
+        }
     }
 
     public int health
@@ -46,12 +56,54 @@ public class Ninja : MonoBehaviour, IAbilities, IStats
         set { m_health = value; }
     }
     
+    
 }
-public class Duck : MonoBehaviour, IAbilities
+
+//public class Duck : MonoBehaviour, IAbilities, IStats
+//{
+ 
+//    public int health
+//    {
+//        get{ return health; }
+//        set{ health = value; }
+//    }
+
+//    public void Health(int m_healthbar)
+//    {
+        
+//    }
+
+//    public void Movement(Vector3 s_move)
+//    {
+      
+//    }
+
+//    public void Stealth()
+//    {
+//        Console.WriteLine(".... Quack");
+//        return;
+
+//    }
+
+//    int IAbilities.Health(int m_healthbar)
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
+
+namespace MyMain
 {
-    public void Stealth()
+    class MainClass
     {
-        Console.WriteLine(".... Quack");
-        return;
+        public static void Main(string[] args)
+        {
+            Ninja Player1 = new Ninja();
+            Ninja Player2 = new Ninja();
+
+
+
+            Console.WriteLine(Player1.Health(Convert.ToInt32 (Console.ReadLine())) - Player2.Health(Convert.ToInt32 ( Console.ReadLine())));
+            Console.ReadKey();
+        }
     }
 }
